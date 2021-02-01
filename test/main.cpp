37e31605741a965bd3e148159b6f220c987738d0
Vasiliy-Pot <78041826+Vasiliy-Pot@users.noreply.github.com>
2021-02-01 15:14:52 +0200

    Update main.cpp

--- test/main.cpp
+++ test/main.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
+#include <stdlib.h>
 using namespace std;
-
-int main() {
-	int a,b,c;
-	cout<<"Input two variables:"<<endl;
-	cin>>a>>b;
-	c=a*b;
-	cout<<"a*b="<<c<<endl;
+int main()
+{
+	int n;
+	n = 40;
+	cout<< n <<endl;
+	cout<< "Hello World!" <<endl;
+	cout<< "How are you?” <<endl;
+	cout<< "All right, Good Bye!" <<endl;
+	system("pause");
 	return 0;
 }
+
 
