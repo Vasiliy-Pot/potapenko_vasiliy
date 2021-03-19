32bf5e03c48a10086e3a68e2c0bebf7c44e73abc
Vasiliy-Pot <78041826+Vasiliy-Pot@users.noreply.github.com>
2021-03-19 13:14:56 +0200

    Add files via upload

---
+++ Task 1/main.cpp
@@ -0,0 +1,15 @@
+/*#include <cstdlib>
+#include <iostream>
+#include <cmath>
+/* variant ¹ 14, student of group 110, name: Potapenko Vasiliy Kostyantinovich */
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    R = (sin(pow(pow(x,2)+4),3)+4.3)/(pow(sin(pow(x,4)),3))
+1. pow(x,2)+4
+2. sin(pow(pow(x,2)+4),3)
+3. sin(pow(pow(x,2)+4),3)+4.3
+4. pow(sin(pow(x,4)),3)
+5. (sin(pow(pow(x,2)+4),3)+4.3)/(pow(sin(pow(x,4)),3))
+}
